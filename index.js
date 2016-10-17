076e740f3a8a3accf12b1aed54383ac126bdfd8c
Sam <contactsamie@gmail.com>
2016-10-17 12:47:11 -0400

    update

---
+++ index.js
@@ -0,0 +1,28 @@
+﻿(function ($, window, undefined) {
+	if(!window.signalx){
+		throw "Please include signalx script first";
+		return;
+		}
+      signalx.server = function (n, f) {        
+        if (n && f) {
+            haservers = true;
+            var nname = toCamelCase(n);
+            signalx.server[nname] = (function (f, n) {
+                return function (message, retTo, sender, messageId) {
+                  return  chatserversend(n, message, retTo, sender, messageId,f);
+                };
+            })(f, nname);
+            nname = toUnCamelCase(n);
+            signalx.server[nname] = (function (f, n) {
+                return function (message, retTo, sender, messageId) {
+                    return chatserversend(n, message, retTo, sender, messageId, f);
+                };
+            })(f, nname);
+        } else {
+            signalx.error.f({
+                description: "error registering server handler"
+            });
+        }
+    };
+ 
+}(window.jQuery, window));
